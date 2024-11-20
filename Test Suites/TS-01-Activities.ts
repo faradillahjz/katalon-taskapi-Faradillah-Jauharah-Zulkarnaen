a1693acce0b163aa5e90509315f1080de5465a39
ASUS_X441M <ASUS_X441M@DESKTOP-BJUULCE>
2024-11-21 06:01:18 +0700

    API Project

---
+++ Test Suites/TS-01-Activities.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-01-Activities</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>faradillahj146@gmail.com;</mailRecipient>
+   <numberOfRerun>2</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0a32f1a1-99ac-4956-9f6e-0aad4abf0730</testSuiteGuid>
+   <testCaseLink>
+      <guid>b541d001-ce19-4c78-87f7-f4823092c953</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-01-Activities All Request</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>106555ca-ff60-43a6-9aee-caadaf813ef8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-02-Activities Get All Data</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f93cd5a3-8073-49a4-88ab-4a8b3e6dc0b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-03-Activities Post Data</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
